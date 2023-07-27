de6d7b52230ca4bf158b1922cb7151c4b17897b4
Rajpote <rrajpote@gmail.com>
2023-07-27 21:48:18 +0545

    commit message

---
+++ calcu.php
@@ -0,0 +1,27 @@
+<?php
+
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$cal = $_POST['cal'];
+
+switch ($cal) {
+    case "+":
+        $value = $num1 + $num2;
+        break;
+    case "-":
+        $value = $num1 - $num2;
+        break;
+    case "*":
+        $value = $num1 * $num2;
+        break;
+    case "/":
+        $value = $num1 / $num2;
+        break;
+
+    default:
+        $value = "error";
+        break;
+}
+
+echo "The output is " . $value;
+?>
