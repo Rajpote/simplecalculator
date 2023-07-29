22bf4ecb32671730ef1da9bf8478513c1818c238
Rajpote <rrajpote@gmail.com>
2023-07-29 22:01:21 +0545

    css

--- form.php
+++ form.php
@@ -3,21 +3,49 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator</title>
+    <style>
+        form{
+            display: grid;
+            justify-content: center;
+        }
+        .container{
+            padding: 25px;
+            margin: 100px 25px 25px 25px;
+            border: 1px solid black;
+        }
+        .operation{
+            display: flex;
+            place-content: center;
+            column-gap: 15px;
+            margin: 10px 0;
+        }
+    </style>
+
 </head>
+
 <body>
     <form action="calcu.php" method="post">
-        <input type="number" name="num1" placeholder="Enter your 1st number"><br>
-        <input type="number" name="num2" placeholder="Enter your 2nd number"><br>
-        
-        <input type="submit" name="cal" value="+">
-        <input type="submit" name="cal" value="-">
-        <input type="submit" name="cal" value="*">
-        <input type="submit" name="cal" value="/"><br>
-        <input type="number" name="result" id="result " placeholder="<?php echo $value?>"><br>
+        <div class="container">
+            <div class="inputfield">
+                <input type="number" name="num1" placeholder="Enter your 1st number"><br>
+                <input type="number" name="num2" placeholder="Enter your 2nd number"><br>
+            </div>
+            <div class="operation">
+                <input type="submit" name="cal" value="+">
+                <input type="submit" name="cal" value="-">
+                <input type="submit" name="cal" value="*">
+                <input type="submit" name="cal" value="/"><br>
+            </div>
+            <div class="output">
+                <input type="number" name="result" id="result " placeholder="<?php echo $value ?>"><br>
+            </div>
+        </div>
     </form>
 </body>
+
 </html>
